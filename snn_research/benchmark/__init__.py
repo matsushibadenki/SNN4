e7b00bab8a7f7d0ce0341dffcb77e33ad650ae84
matsushibadenki <littlebuddha@mac.com>
2025-10-12 20:16:51 +0900

    Update __init__.py

--- snn_research/benchmark/__init__.py
+++ snn_research/benchmark/__init__.py
@@ -1,6 +1,6 @@
-# matsushibadenki/snn4/SNN4-6a53b6b567b4ca70949a9276cacae4f9ee0ee306/snn_research/benchmark/__init__.py
+# matsushibadenki/snn4/snn_research/benchmark/__init__.py
 # (修正)
-# 修正: ImportErrorを解消するため、TASK_REGISTRYを__all__に追加。
+# 修正: ImportErrorを解消するため、TASK_REGISTRYを__all__から削除。
 
 from .ann_baseline import ANNBaselineModel
 from .tasks import SST2Task
