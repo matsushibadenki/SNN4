f4ddd97985a3a988bb1202d3f2797172639cccac
matsushibadenki <littlebuddha@mac.com>
2025-10-12 20:29:45 +0900

    Update __init__.py

--- snn_research/benchmark/__init__.py
+++ snn_research/benchmark/__init__.py
@@ -1,9 +1,13 @@
 # matsushibadenki/snn4/snn_research/benchmark/__init__.py
 # (修正)
-# 修正: ImportErrorを解消するため、TASK_REGISTRYを__all__から削除。
+# 修正: 循環参照を解消するため、TASK_REGISTRYをここで定義する。
 
 from .ann_baseline import ANNBaselineModel
 from .tasks import SST2Task
 from .metrics import calculate_accuracy
 
-__all__ = ["ANNBaselineModel", "SST2Task", "calculate_accuracy"]
+TASK_REGISTRY = {
+    "sst2": SST2Task,
+}
+
+__all__ = ["ANNBaselineModel", "SST2Task", "calculate_accuracy", "TASK_REGISTRY"]
