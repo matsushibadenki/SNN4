b7578f438bbf21b82486c9dfc2a7c7a2d7ab6441
matsushibadenki <littlebuddha@mac.com>
2025-10-12 19:58:39 +0900

    Update __init__.py

--- snn_research/benchmark/__init__.py
+++ snn_research/benchmark/__init__.py
@@ -1,7 +1,9 @@
 # matsushibadenki/snn4/snn_research/benchmark/__init__.py
+# (修正)
+# 修正: ImportErrorを解消するため、TASK_REGISTRYを__all__に追加。
 
 from .ann_baseline import ANNBaselineModel
-from .tasks import SST2Task
+from .tasks import SST2Task, TASK_REGISTRY
 from .metrics import calculate_accuracy
 
-__all__ = ["ANNBaselineModel", "SST2Task", "calculate_accuracy"]
+__all__ = ["ANNBaselineModel", "SST2Task", "calculate_accuracy", "TASK_REGISTRY"]
