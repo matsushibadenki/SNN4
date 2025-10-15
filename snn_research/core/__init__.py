213e54335cb662c51c06e5f8b6af81d16dd47a18
matsushibadenki <littlebuddha@mac.com>
2025-10-15 17:58:53 +0900

    Update __init__.py

--- snn_research/core/__init__.py
+++ snn_research/core/__init__.py
@@ -4,12 +4,14 @@
 # Description:
 # - 循環インポートを解消するため、BaseModelを新しい `base` モジュールからインポートする。
 # - 新しいSpikingHRMモデルを公開する。
+# - snnTorchベースのモデルを公開する。
 
 from .base import BaseModel
 from .snn_core import SNNCore, BreakthroughSNN, SpikingTransformer, SimpleSNN
 from .mamba_core import SpikingMamba
 from .hrm_core import SpikingHRM
 from .neurons import AdaptiveLIFNeuron, IzhikevichNeuron
+from .sntorch_models import SpikingTransformerSnnTorch
 
 __all__ = [
     "BaseModel",
@@ -21,5 +23,5 @@ __all__ = [
     "SimpleSNN",
     "AdaptiveLIFNeuron",
     "IzhikevichNeuron",
+    "SpikingTransformerSnnTorch",
 ]
-
