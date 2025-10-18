31ffc7966a628e6db9e1121f8f3fe568565e194a
matsushibadenki <littlebuddha@mac.com>
2025-10-19 06:57:16 +0900

    Update __init__.py

--- snn_research/core/__init__.py
+++ snn_research/core/__init__.py
@@ -1,14 +1,9 @@
 # ファイルパス: snn_research/core/__init__.py
-# (修正)
-# Title: Core SNNモジュール
-# Description:
-# - 循環インポートを解消するため、BaseModelを新しい `base` モジュールからインポートする。
-# - SpikingHRMをTinyRecursiveModelに置き換えて公開する。
 
 from .base import BaseModel
 from .snn_core import SNNCore, BreakthroughSNN, SpikingTransformer, SimpleSNN
 from .mamba_core import SpikingMamba
-from .hrm_core import TinyRecursiveModel
+from .trm_core import TinyRecursiveModel
 from .neurons import AdaptiveLIFNeuron, IzhikevichNeuron
 from .sntorch_models import SpikingTransformerSnnTorch
 
